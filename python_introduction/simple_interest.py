b000eea434084cbecc3b4953643dcbb11f3686aa
Ebah Obruthe <ebahobruthe@gmail.com>
2025-09-13 13:41:33 +0100

    Updated simple_interest file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,11 @@
 
 # This script performs simple interest operation operations
 
-p = 1000
-r = 0.05 
-t = 3
+principal = 1000
+rate = 0.05 
+time = 3
 
 #simple interest formula
-i = p * r * t
+interest = principal * rate * time
 
-print(f"The simple interest is {i}")
+print(f"The simple interest is {interest}")
