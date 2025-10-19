ef2fb0b95470d1eface9ffdad1203a7bece47d6d
MrBruth <ebahobruthe@gmail.com>
2025-10-19 13:04:16 +0100

    Add programming_paradigm folder with bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, account_holder, initial_balance=0):
+        self.account_holder = account_holder
+        self.__balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+            self.balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__balance:.2f}")
+
+    def get_balance(self):
+        return self.__balance
+    
